8bfc70e237584899274c04d55477b47eb04e5b2c
unknown <ioanlabici@gmail.com>
2026-04-15 08:21:06 +0300

    B-1: Published lang directory in laravel 12.

---
+++ BE/lang/en/messages.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'soft_deleted_resource' => 'This resource has been soft deleted. You may restore it within 30 days. After this period, it will be permanently deleted and cannot be recovered.',
+    'delete_failed' => 'The resource could not be deleted. Please try again later.',
+];
