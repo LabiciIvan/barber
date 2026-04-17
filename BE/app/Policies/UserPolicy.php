6ca6f07186b659e15494faf64ea3189a73d52f54
unknown <ioanlabici@gmail.com>
2026-04-17 08:25:08 +0300

    B-1: User policy.

---
+++ BE/app/Policies/UserPolicy.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Shop;
+use App\Models\User;
+use Illuminate\Support\Facades\Log;
+
+class UserPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
