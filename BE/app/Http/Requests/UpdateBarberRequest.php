1fb097d714a165d1f2aaf24f2ea704c81e0dc913
unknown <ioanlabici@gmail.com>
2026-04-30 08:36:24 +0300

    B-1: Barber update request.

---
+++ BE/app/Http/Requests/UpdateBarberRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateBarberRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email'     => 'sometimes|required|email|unique:users,email',
+            'password'  => 'sometimes|required',
+            'shop_id'   => 'sometimes|required|exists:shop,id',
+            'phone'     => 'sometimes|required|numeric',
+        ];
+    }
+}
