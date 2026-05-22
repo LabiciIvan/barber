bedc171f2540bad0cf893fb4ce777ef9302d5bc8
unknown <ioanlabici@gmail.com>
2026-05-22 09:07:52 +0300

    B-1: User controller.

--- BE/app/Http/Controllers/UserController.php
+++ BE/app/Http/Controllers/UserController.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Services\UserService;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
 class UserController extends Controller
 {
-    //
+    public function show(UserService $userService)
+    {
+        return $userService->show(Auth::user());
+    }
 }
