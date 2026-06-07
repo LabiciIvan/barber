2d76e48c08e73f20d5c1b19b3e38be4a686e3d43
unknown <ioanlabici@gmail.com>
2026-06-07 08:45:11 +0300

    B-1: Shop policy rule.

--- BE/app/Policies/ShopPolicy.php
+++ BE/app/Policies/ShopPolicy.php
@@ -4,6 +4,7 @@ namespace App\Policies;
 
 use App\Models\Shop;
 use App\Models\User;
+use Illuminate\Support\Facades\Log;
 
 class ShopPolicy
 {
@@ -25,5 +26,34 @@ class ShopPolicy
     public function restore(User $user): bool
     {
         return $user->role === 'admin';
+    }
+
+    public function createBarber(User $user, Shop $shop)
+    {
+        return ($user->role === 'admin' || ($user->role === 'owner' && $user->shop_id === $shop->id));
+    }
+
+    /**
+     * Update applies only to user with barber role or an admin.
+     */
+    public function updateBarber(User $user, Shop $shop, User $barber)
+    {
+        if ($barber->role != 'barber' || ($user->role === 'admin' && $user->id === $barber->id)) {
+            return false;
+        }
+
+        return ($user->role === 'admin' || ($user->role === 'barber' && $user->id === $barber->id && $barber->shop_id === $shop->id));
+    }
+
+    /**
+     * Owner of the shop can delete it's own barbers accounts only.
+     */
+    public function deleteBarber(User $user, Shop $shop, User $barber)
+    {
+        if ($barber->role != 'barber' || ($user->role != 'owner' && $user->shop_id === $shop->id)) {
+            return false;
+        }
+
+        return $barber->shop_id === $shop->id;
     }
 }
