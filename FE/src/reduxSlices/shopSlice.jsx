21f2f723366c4e8d1787b88175d0d7dde68388b6
unknown <ioanlabici@gmail.com>
2026-06-08 09:08:33 +0300

    B-1: new redux slice.

---
+++ FE/src/reduxSlices/shopSlice.jsx
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    shops: null,
+}
+
+const shopSlice = createSlice({
+    name: 'shop',
+    initialState,
+    reducers: {
+        setShops: (state, action) => {
+            state.shops = action.payload;
+        }
+    }
+});
+
+// Export selectors
+export const selectorShops = (state) => {state.shop.shops}
+
+// Export actions
+export const {setShops} = shopSlice.actions;
+
+
+// Default named exporter
+export default shopSlice.reducer;
