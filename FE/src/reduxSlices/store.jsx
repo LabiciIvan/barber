111cf4f182d814ba0da715abc1100ee896d8ea49
unknown <ioanlabici@gmail.com>
2026-08-01 09:31:49 +0300

    B-1: Redux store update.

--- FE/src/reduxSlices/store.jsx
+++ FE/src/reduxSlices/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './authSlice';
 import shopReducer from './shopSlice';
+import counterReducer from './counterSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     shop: shopReducer,
+    counter: counterReducer
   },
 });
